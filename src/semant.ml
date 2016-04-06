d269d013a5a73ffd76c4bca9060df9bbde5d1454
BernardoAbreu <bernardoaabreu@gmail.com>
2016-04-06 01:14:44 -0400

    a porra do semant Felipe

--- src/semant.ml
+++ src/semant.ml
@@ -15,13 +15,22 @@ let check (statements, functions, tuples) =
 
   (*raise( Failure( "not implemented" ));;*)
 
-  (* Function declaration for named functions *)
-  let built_in_decls =  StringMap.singleton "print"
-   { rtyp = Void; fname = "print"; formals = [(String, "x")];
-     locals = []; body = [] }
+  (* Raise an exception if the given list has a duplicate *)
+  let report_duplicate exceptf list =
+    let rec helper = function
+  n1 :: n2 :: _ when n1 = n2 -> raise (Failure (exceptf n1))
+      | _ :: t -> helper t
+      | [] -> ()
+    in helper (List.sort compare list)
   in
 
-  let function_decls = List.fold_left (fun m fd -> StringMap.add fd.fname fd m)
+  (* Function declaration for named functions *)
+  let built_in_decls =  StringMap.singleton "print"
+   { rtyp = Void; semfname = "print"; semformals = [(String, "x")];
+     semlocals = []; sembody = [] }
+  in
+
+  let function_decls = List.fold_left (fun m fd -> StringMap.add fd.semfname fd m)
                        built_in_decls functions
   in
 
@@ -31,26 +40,22 @@ let check (statements, functions, tuples) =
 
   let check_function func =
 
-    report_duplicate (fun n -> "duplicate formal " ^ n ^ " in " ^ func.fname)
-      (List.map snd func.formals);
+    report_duplicate (fun n -> "duplicate formal " ^ n ^ " in " ^ func.semfname)
+      (List.map snd func.semformals);
 
-    report_duplicate (fun n -> "duplicate local " ^ n ^ " in " ^ func.fname)
-      (List.map snd func.locals);
+    report_duplicate (fun n -> "duplicate local " ^ n ^ " in " ^ func.semfname)
+      (List.map snd func.semlocals);
 
       (* Return the type of an expression or raise an exception*)
       let rec expr = function
             SStrLit _ -> String
-          | SCall(fname, actuals) as call -> let fd = function_decl fname in
-           if List.length actuals != List.length fd.formals then
+          | SCall(fname, actuals) -> let fd = function_decl fname in
+           if List.length actuals != List.length fd.semformals then
              raise (Failure ("expecting " ^ string_of_int
-               (List.length fd.formals) ^ " arguments in " ^ string_of_expr call))
+               (List.length fd.semformals) ^ " arguments in " ))
            else
-             List.iter2 (fun (ft, _) e -> let et = expr e in
-                ignore (check_assign ft et
-                  (Failure ("illegal actual argument found " ^ string_of_typ et ^
-                  " expected " ^ string_of_typ ft ^ " in " ^ string_of_expr e))))
-               fd.formals actuals;
-             fd.typ
+             fd.rtyp
+          | _ -> raise( Failure( "not implemented" ))
       in
 
       (* Verify a statement or throw an exception *)
@@ -63,9 +68,10 @@ let check (statements, functions, tuples) =
                | [] -> ()
               in check_block sl
           | SExpr e -> ignore (expr e)
+          | _ -> raise( Failure( "not implemented" ))
       in
 
-      stmt (SBlock func.body)
+      stmt (SBlock func.sembody)
 
   in
   List.iter check_function functions

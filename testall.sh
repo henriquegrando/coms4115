04989fc57ac1c83217039d0d34dfd6fde6440b13
Hugo Araujo de Sousa <ha2398@columbia.edu>
2016-04-12 23:03:05 -0400

    Test Script Generating .diff files correctly

--- testall.sh
+++ testall.sh
@@ -7,6 +7,9 @@
 
 # Path to C Compiler (GCC)
 GCC="gcc"
+LDFLAG="-g -Llibs/"
+CFLAG="-Ilibs/"
+LDLIB="-ldamplio"
 #GCC="usr/bin/gcc"
 
 # DaMPL compiler.
@@ -15,7 +18,10 @@ DAMPLC="./dampl"
 Build() {
 	cd src &&
 	make &&
-	cp dampl ../dampl
+	cp dampl ../dampl 
+	cd ..
+	cd libs &&
+	make &&
 	cd ..
 }
 
@@ -31,5 +37,12 @@ for file in $files
 do
 	filename="${file%.*}"
 	eval "$DAMPLC" "<" "${file}" ">" "${filename}.c"
-	eval "$GCC" "-o" "${filename}" "${filename}.c"
+	eval "$GCC" "-o" "${filename}" "${filename}.c" "$CFLAG" "$LDFLAG" "$LDLIB"
+
+	./${filename} > ./tests/temp.out
+
+	diff -b ${filename}.out temp.out > ${filename}.diff 1>&2
+
+	rm ./tests/temp.out
+	rm ${filename}
 done

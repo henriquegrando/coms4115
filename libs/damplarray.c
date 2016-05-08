93ca7825cb0e57712c528e2cb2358c242ea9afb0
Henrique Pizzol Grando <hp2409@columbia.edu>
2016-05-08 00:09:27 -0400

    Implements part of array interface

--- libs/damplarray.c
+++ libs/damplarray.c
@@ -1,3 +1,64 @@
 #include "dampllib.h"
 #include <stdio.h>
 
+/* Array constructor for every type */
+
+Array dampl_arr_new__arr ()
+{
+    Array arr;
+
+    arr.a = (Array *) calloc(INITIAL_SIZE, sizeof(Array));
+    arr.size = INITIAL_SIZE;
+    arr.add = &dampl_arr_add__arr;
+    arr.insert = &dampl_arr_insert__arr;
+    arr.set = &dampl_arr_set__arr;
+    arr.get = &dampl_arr_get__arr;
+
+    return arr;
+}
+
+Array dampl_arr_new__int ()
+{
+    Array arr;
+
+    arr.a = (int *) calloc(INITIAL_SIZE, sizeof(int));
+    arr.size = INITIAL_SIZE;
+    arr.add = &dampl_arr_add__int;
+    arr.insert = &dampl_arr_insert__int;
+    arr.set = &dampl_arr_set__int;
+    arr.get = &dampl_arr_get__int;
+
+    return arr;
+
+}
+
+
+Array dampl_arr_new__float ()
+{
+    Array arr;
+
+    arr.a = (float *) calloc(INITIAL_SIZE, sizeof(float));
+    arr.size = INITIAL_SIZE;
+    arr.add = &dampl_arr_add__float;
+    arr.insert = &dampl_arr_insert__float;
+    arr.set = &dampl_arr_set__float;
+    arr.get = &dampl_arr_get__float;
+
+    return arr;
+}
+
+Array dampl_arr_new__str ()
+{
+    Array arr;
+
+    arr.a = (String *) calloc(INITIAL_SIZE, sizeof(String));
+    arr.size = INITIAL_SIZE;
+    arr.add = &dampl_arr_add__str;
+    arr.insert = &dampl_arr_insert__str;
+    arr.set = &dampl_arr_set__str;
+    arr.get = &dampl_arr_get__str;
+
+    return arr;
+}
+
+//Array dampl_arr_new__tup ();
--- libs/damplarray.h
+++ libs/damplarray.h
@@ -1,30 +1,48 @@
 #ifndef _DAMPL_ARRAY_
 #define _DAMPL_ARRAY_
 
+#define INITIAL_SIZE 5
+
 /* Array structure */
 
 typedef struct
 {
     void * a;
     int size;
+    void (*add) (Array *, void *);
     void (*insert) (Array *, int, void *);
-    void (*append) (Array *, void *);
-    void (*get) (Array, int);
+    void (*set) (Array *, int, void *);
+    void * (*get) (Array, int);
 }Array;
 
 /* Array constructor for every type */
 
-Array dampl_arr_new__arr (Array);
+Array dampl_arr_new__arr ();
 
-Array dampl_arr_new__int (int *);
+Array dampl_arr_new__int ();
 
-Array dampl_arr_new__float (float *);
+Array dampl_arr_new__float ();
 
-Array dampl_arr_new__str (String *);
+Array dampl_arr_new__str ();
 
-//Array dampl_arr_new__tup (Tuple * a);
+//Array dampl_arr_new__tup ();
 
-/* Array insertion method */
+/* Array add method 
+   Appends an element in the end of the array */
+
+void dampl_arr_add__arr (Array *, Array);
+
+void dampl_arr_add__int (Array *, int);
+
+void dampl_arr_add__float (Array *, float);
+
+void dampl_arr_add__str (Array *, String);
+
+//void dampl_arr_add__tup (Array *, Array);
+
+
+/* Array insertion method 
+   Insert an element in a given position, shifting all elements after it */
 
 void dampl_arr_insert__arr (Array *, int, Array);
 
@@ -36,19 +54,21 @@ void dampl_arr_insert__str (Array *, int, String);
 
 //void dampl_arr_insert__tup (Array *, int, Tuple *);
 
-/* Array append method */
+/* Array set method
+   Set a given position in an array to the value passed by the user */
 
-void dampl_arr_append__arr (Array *, Array);
+void dampl_arr_set__arr (Array *, int, Array);
 
-void dampl_arr_append__int (Array *, int); 
+void dampl_arr_set__int (Array *, int, int); 
 
-void dampl_arr_append__float (Array *, float); 
+void dampl_arr_set__float (Array *, int, float); 
 
-void dampl_arr_append__str (Array *, String); 
+void dampl_arr_set__str (Array *, int, String); 
 
-//void dampl_arr_append__tup (Array *, Tuple *); 
+//void dampl_arr_set__tup (Array *, Tuple *); 
 
-/* Array get method */
+/* Array get method 
+   Returns an element in a given position */
 
 Array dampl_arr_get__arr (Array, int);
 
@@ -59,5 +79,17 @@ float dampl_arr_get__float (Array, int);
 String dampl_arr_get__str (Array, int);
 
 //Tuple dampl_arr_get__tup (Array, int);
+
+/* Array ensure capacity method */
+
+void dampl_arr_ensure_cap__arr (Array *, int);
+
+void dampl_arr_ensure_cap__int (Array *, int);
+
+void dampl_arr_ensure_cap__float (Array *, int);
+
+void dampl_arr_ensure_cap__str (Array *, int);
+
+//void dampl_arr_ensure_cap__tup (Array *, int);
 
 #endif

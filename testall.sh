1c1ca882d6820cfcec6fe44b0516cc6d8631db5f
Hugo Sousa <ha2398@columbia.edu>
2016-05-08 21:18:21 -0400

    Test Script and Tests Folder

---
+++ testall.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# Regression testing script for DaMPL Compiler
+# Step through a list of files
+#  Compile, run, and check the output of each expected-to-work test
+#  Compile and check the error of each expected-to-fail test
+
+# Path to C Compiler (GCC)
+GCC="gcc"
+LDFLAG="-g -Llibs/"
+CFLAG="-Ilibs/"
+LDLIB="-ldampllib"
+#GCC="usr/bin/gcc"
+
+# DaMPL compiler.
+DAMPLC="./dampl"
+
+Build() {
+	cd src &&
+	make &&
+	cp dampl ../dampl 
+	cd ..
+	cd libs &&
+	make &&
+	cd ..
+}
+
+# Main #
+
+# Builds the compiler
+Build
+
+# Test files
+files="tests/*.mpl"
+
+for file in $files
+do
+	filename="${file%.*}"
+	eval "$DAMPLC" "<" "${file}" ">" "${filename}.c"
+	eval "$GCC" "-o" "${filename}" "${filename}.c" "$CFLAG" "$LDFLAG" "$LDLIB"
+
+	./${filename} > ./tests/temp.out
+
+	diff -b ${filename}.out ./tests/temp.out > ${filename}.diff 1>&2
+
+	rm ./tests/temp.out
+	rm ${filename}
+done
